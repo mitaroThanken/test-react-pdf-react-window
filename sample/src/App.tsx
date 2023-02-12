91d39ad23ea426b1f1da4c59a1a3c8165d049f1e
YAMAMI Taro <mitarothanken@gmail.com>
2023-02-12 22:42:19 +0900

    [FIX] Edge だと横方向のスクロールバーが表示されてしまう
    縦方向のスクロールバーが VariableSizeList の width 「内に」表示され、
    結果、Row で描画しているコンテンツに対し「幅が足りなくなる」ため。
    
    スクロールバーの幅をあらかじめ求めておき、
    Row で描画するコンテンツの幅を調整するようにする。
    この時、「高さの計算に対しても調整が必要」であることに注意。

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -1,5 +1,5 @@
 import { asyncMap } from '@wojtekmaj/async-array-utils';
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { pdfjs, Document, Page } from 'react-pdf/dist/esm/entry.vite';
 import { VariableSizeList } from 'react-window';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
@@ -7,6 +7,7 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 import './App.css'
 import { LoadingProcessData } from 'react-pdf';
 
+/** スクロールバーを含む幅 */
 const width = 500;
 const height = width * 1.5;
 
@@ -14,7 +15,7 @@ interface RowProps {
   index: number,
   style: React.CSSProperties
 }
-const Row = (props: RowProps) => {
+const Row = (width: number) => (props: RowProps) => {
   const onPageRenderSuccess = (page: pdfjs.PDFPageProxy) => {
     console.info(`Page ${page.pageNumber} rendered.`);
   };
@@ -60,17 +61,53 @@ function App() {
     setPdf(nextPdf);
   }, []);
 
+  // スクロールバーの幅・高さを捉える
+  // cf. https://stackoverflow.com/a/3417992
+  const [scrollBarSize, setScrollBarSize] = useState<number[]>();
+  useEffect(() => {
+    const inner = document.createElement('p');
+    inner.style.width = "100%";
+    inner.style.height = "100%";
+
+    const outer = document.createElement('div');
+    outer.style.position = "absolute";
+    outer.style.top = "0px";
+    outer.style.left = "0px";
+    outer.style.visibility = "hidden";
+    outer.style.width = "100px";
+    outer.style.height = "100px";
+    outer.style.overflow = "hidden";
+    outer.appendChild(inner);
+
+    document.body.appendChild(outer);
+
+    const w1 = inner.offsetWidth;
+    const h1 = inner.offsetHeight;
+    outer.style.overflow = 'scroll';
+    let w2 = inner.offsetWidth;
+    let h2 = inner.offsetHeight;
+    if (w1 == w2) w2 = outer.clientWidth;
+    if (h1 == h2) h2 = outer.clientHeight;
+
+    document.body.removeChild(outer);
+
+    setScrollBarSize([(w1 - w2), (h1 - h2)]);
+  }, []);
+
+  /** スクロールバーを除いた、表示に使用できる幅 */
+  const effectiveWidth = useMemo(() => !scrollBarSize ? width : width - scrollBarSize[0], [scrollBarSize]);
+
   const getPageHeight = useCallback((pageIndex: number) => {
     if (!pageViewports) {
       throw new Error("getPageHeight() called too early");
     }
 
     const pageViewport = pageViewports[pageIndex];
-    const scale = width / pageViewport.width;
+    const scale = effectiveWidth / pageViewport.width;
     const actualHeight = pageViewport.height * scale;
 
     return actualHeight;
-  }, [pageViewports]);
+  }, [pageViewports, effectiveWidth]);
 
   const onDocumentLoadProgress = ({ loaded, total }: LoadingProcessData) => {
     const tot = Math.round((loaded / total) * 100);
@@ -94,7 +131,7 @@ function App() {
               height={height}
               itemCount={pdf.numPages}
               itemSize={getPageHeight}>
-              {Row}
+              {Row(effectiveWidth)}
             </VariableSizeList>
           )
           : <></>}
